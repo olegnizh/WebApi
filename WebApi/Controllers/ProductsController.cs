c25e6b45f131e55804ccb4e552a1d5d53f4bb890
olegnizh <olegnizh@mail.ru>
2024-07-17 18:12:11 +0300

    Добавьте файлы проекта.

---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+namespace WebApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WebApi.Entities;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private List<Product> _products = new List<Product>
+    {
+        new Product { Id = 1, Name = "Alex" },
+        new Product { Id = 2, Name = "Bill" }
+    };
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_products);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetById(int id)
+    {
+        var product = _products.Find(x => x.Id == id);
+        if (product == null)
+            return NotFound();
+
+        return Ok(product);
+    }
+}
